e5e9d3112cf6c03033b8174ae0e2b4373da5e89f
geni <gnaneswari.lolugu@gmail.com>
2025-04-03 12:27:43 -0400

    one on one call working

--- server/server.js
+++ server/server.js
@@ -42,36 +42,34 @@ let producerTransport
 let consumerTransport
 let producer
 let consumer
+let transports = new Map()
+let producers = new Map()
+let consumers = new Map()
+let room;
 io.on("connection", socket =>{
   
   console.log('new peer connected', socket.id)
   socket.on('joinRoom', async ({ username, roomId }, callback) => {
-    let room = rooms.get(roomId)
-    
-
+    room = rooms.get(roomId)
     if(!room){
       console.log('creating a new room with id:', roomId, `Adding user:${username}`)
       router = await getRouter();
       rooms.set(roomId, { router, peers: [] });
-      async()=>{
-        if(rooms.has(roomId)){
-          await rooms.peers.push(username)
-        }
+      room = rooms.get(roomId)
+      if(rooms.has(roomId)){
+        room.peers.push(username)
       }
     }
     else{
       console.log(`Adding ${username} to existing room ${roomId}`)
       router = room.router
-      async()=>{
-        if(rooms.has(roomId)){
-          await rooms.peers.push(username)
-          socket.emit("newParticipant", rooms.peers)
-          console.log("emiting event new participant and sending peers:", rooms.peers)
-        }
+      if(rooms.has(roomId)){
+        room = rooms.get(roomId)
+        room.peers.push(username)
+        socket.emit("newParticipant", room.peers)
+        console.log("emiting event new participant and sending peers:", room.peers)
       }
     }
-    //push the new peer into the room
-    console.log(rooms)
     
     //send router rtpcapabilities to client
     const  rtpCapabilities= router.rtpCapabilities
@@ -81,6 +79,10 @@ io.on("connection", socket =>{
       // create both producerTransport and consumer Transport
       producerTransport = await createWebRtcTransport(router)
       consumerTransport = await createWebRtcTransport(router)
+      transports.set(`${username}`, { producer: producerTransport, consumer: consumerTransport})
+      console.log('transports for current user stored in map')
+      producers.set(`${username}`,[])
+      consumers.set(`${username}`, [])
       const producerOptions = {
         id: producerTransport.id,
         iceParameters: producerTransport.iceParameters,
@@ -110,9 +112,14 @@ io.on("connection", socket =>{
       kind,
       rtpParameters,
     })
-
+    
     console.log('Producer', producer.id, producer.kind)
-
+    const cur_producers= producers.get(username)
+    if(cur_producers){
+      cur_producers.push(producer)
+      producers.set(username, cur_producers)
+    }
+    console.log('producer from map', producers.get(username))
     producer.on('transportclose', () => {
       console.log('transport for this producer closed ')
       producer.close()
@@ -127,42 +134,61 @@ io.on("connection", socket =>{
   // see client's socket.emit('transport-recv-connect', ...)
   socket.on('transport-recv-connect', async ({ dtlsParameters }) => {
     console.log(`DTLS PARAMS: ${dtlsParameters}`)
-    await consumerTransport.connect({ dtlsParameters })
+    const currentTransports = transports.get(username)
+    await currentTransports.consumer.connect({ dtlsParameters })
   })
 
   socket.on('consume', async ({ rtpCapabilities }, callback) => {
     try {
       // check if the router can consume the specified producer
-      if (router.canConsume({
-        producerId: producer.id,
-        rtpCapabilities
-      })) {
-        // transport can now consume and return a consumer
-        console.log('router can consume')
-        consumer = await consumerTransport.consume({
-          producerId: producer.id,
-          rtpCapabilities,
-          paused: true,
-        })
-        consumer.on('transportclose', () => {
-          console.log('transport close from consumer')
-        })
-
-        consumer.on('producerclose', () => {
-          console.log('producer of consumer closed')
-        })
-        // from the consumer extract the following params
-        // to send back to the Client
-        const params = {
-          id: consumer.id,
-          producerId: producer.id,
-          kind: consumer.kind,
-          rtpParameters: consumer.rtpParameters,
+      room = rooms.get(roomId)
+      const cur_peers = room.peers
+      console.log("current peers in the room", cur_peers, "cur username", username)
+      cur_peers.forEach(async(peer)=>{
+        if(peer!=username){
+          console.log(" inside if check foreach")
+          const consume_producer = producers.get(peer)[0]
+          if (router.canConsume({
+            producerId: consume_producer.id,
+            rtpCapabilities
+          })) {
+            // transport can now consume and return a consumer
+            console.log('router can consume', 'creating consumer for:', peer)
+            const cur_transports = transports.get(username)
+            consumer = await cur_transports.consumer.consume({
+              producerId: consume_producer.id,
+              rtpCapabilities,
+              paused: true,
+            })
+            const cur_consumers= consumers.get(username)
+            if(cur_consumers){
+              cur_consumers.push(consumer)
+              consumers.set(username, cur_consumers)
+            }
+            console.log('consumer from map', consumers.get(username))
+            
+            consumer.on('transportclose', () => {
+              console.log('transport close from consumer')
+            })
+    
+            consumer.on('producerclose', () => {
+              console.log('producer of consumer closed')
+            })
+            // from the consumer extract the following params
+            // to send back to the Client
+            const params = {
+              id: consumer.id,
+              producerId: consume_producer.id,
+              kind: consumer.kind,
+              rtpParameters: consumer.rtpParameters,
+            }
+            console.log('consumer params', params)
+            // send the parameters to the client
+            callback({ params })
+          }
         }
-        console.log('consumer params', params)
-        // send the parameters to the client
-        callback({ params })
-      }
+      })
+      
     } catch (error) {
       console.log(error.message)
       callback({
