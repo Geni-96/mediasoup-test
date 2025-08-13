8fbb713b1fee49fc10bd4fa43e7c493f9c8f3dfb
geni <gnaneswari.lolugu@gmail.com>
2025-08-12 23:25:19 -0500

    remove the dependancies on redis file for tests and specify node_modules path in jest config

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,11 @@
 module.exports = {
   testEnvironment: 'node',
   testMatch: ['<rootDir>/tests/**/*.test.js'],
+  modulePaths: ['<rootDir>/server/node_modules'],
   roots: ['<rootDir>', '<rootDir>/tests'],
   collectCoverageFrom: ['server/**/*.js', '!server/**/tests/**'],
   coverageDirectory: '<rootDir>/coverage',
   setupFiles: ['dotenv/config'],
   resetMocks: true,
   restoreMocks: true,
-  moduleNameMapper: {
-    '^redis$': '<rootDir>/__mocks__/redis.js'
-  }
 };
